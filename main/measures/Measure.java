740b476132b82d68089a51a910d14c3a17c74ad9
u208490 <urs.beeli@sbb.ch>
2019-02-11 17:04:31 +0100

    Cleaned up measurement/unit exercise

--- main/measures/Measure.java
+++ main/measures/Measure.java
@@ -4,24 +4,6 @@ package measures;
 import java.util.Objects;
 
 public class Measure {
-
-  private static final int TEA_SPOONS_PER_TABLE_SPOON = 3;
-  private static final int TEA_SPOONS_PER_OUNCE = 2 * TEA_SPOONS_PER_TABLE_SPOON;
-  private static final int TEA_SPOONS_PER_CUP = 8 * TEA_SPOONS_PER_OUNCE;
-  private static final int TEA_SPOONS_PER_PINT = 2 * TEA_SPOONS_PER_CUP;
-  private static final int TEA_SPOONS_PER_QUART = 2 * TEA_SPOONS_PER_PINT;
-  private static final int TEA_SPOONS_PER_GALLON = 4 * TEA_SPOONS_PER_QUART;
-
-  public enum Unit {
-    TEA_SPOON,
-    TABLE_SPOON,
-    OUNCE,
-    CUP,
-    PINT,
-    QUART,
-    GALLON;
-  }
-
   private final double value;
   private final Unit unit;
 
@@ -32,57 +14,22 @@ public class Measure {
     this.unit = unit;
   }
 
-  private Measure convertToLowest() {
-    switch (unit) {
-      case TEA_SPOON:
-        return this;
-      case TABLE_SPOON:
-        return new Measure(this.value * TEA_SPOONS_PER_TABLE_SPOON, Unit.TEA_SPOON);
-      case OUNCE:
-        return new Measure(this.value * TEA_SPOONS_PER_OUNCE, Unit.TEA_SPOON);
-      case CUP:
-        return new Measure(this.value * TEA_SPOONS_PER_CUP, Unit.TEA_SPOON);
-      case PINT:
-        return new Measure(this.value * TEA_SPOONS_PER_PINT, Unit.TEA_SPOON);
-      case QUART:
-        return new Measure(this.value * TEA_SPOONS_PER_QUART, Unit.TEA_SPOON);
-      case GALLON:
-        return new Measure(this.value * TEA_SPOONS_PER_GALLON, Unit.TEA_SPOON);
-    }
-    throw new RuntimeException("Unexpected source unit " + unit);
-  }
-
-  private Measure convertFromLowest(Unit targetUnit) {
-    if (unit == Unit.TEA_SPOON) {
-      switch (targetUnit) {
-        case TEA_SPOON:
-          return this;
-        case TABLE_SPOON:
-          return new Measure(this.value / TEA_SPOONS_PER_TABLE_SPOON, targetUnit);
-        case OUNCE:
-          return new Measure(this.value / TEA_SPOONS_PER_OUNCE, targetUnit);
-        case CUP:
-          return new Measure(this.value / TEA_SPOONS_PER_CUP, targetUnit);
-        case PINT:
-          return new Measure(this.value / TEA_SPOONS_PER_PINT, targetUnit);
-        case QUART:
-          return new Measure(this.value / TEA_SPOONS_PER_QUART, targetUnit);
-        case GALLON:
-          return new Measure(this.value / TEA_SPOONS_PER_GALLON, targetUnit);
-      }
-    }
-    throw new RuntimeException("Cannot convertFromLowest for units other than TEA_SPOON. Current unit is " + unit);
-  }
-
   public Measure convertTo(Unit targetUnit) {
-    return this.convertToLowest().convertFromLowest(targetUnit);
+    return new Measure(Unit.convert(this.value, this.unit, targetUnit), targetUnit);
   }
 
   public Measure add(final Measure other, final Unit targetUnit) {
-    return new Measure(
-        this.convertToLowest().value + other.convertToLowest().value,
-        Unit.TEA_SPOON
-    ).convertTo(targetUnit);
+    double teaSpoons = Unit.convert(this.value, this.unit, Unit.TEA_SPOON) +
+        Unit.convert(other.value, other.unit, Unit.TEA_SPOON);
+
+    return new Measure(Unit.convert(teaSpoons, Unit.TEA_SPOON, targetUnit), targetUnit);
+  }
+
+  public Measure subtract(final Measure other, final Unit targetUnit) {
+    double teaSpoons = Unit.convert(this.value, this.unit, Unit.TEA_SPOON) -
+        Unit.convert(other.value, other.unit, Unit.TEA_SPOON);
+
+    return new Measure(Unit.convert(teaSpoons, Unit.TEA_SPOON, targetUnit), targetUnit);
   }
 
   @Override
@@ -94,17 +41,17 @@ public class Measure {
       return false;
     }
     final Measure that = (Measure) other;
-    Measure left = this.convertToLowest();
-    Measure right = that.convertToLowest();
-    return left.value == right.value;
+    return Unit.convert(this.value, this.unit, Unit.TEA_SPOON) ==
+        Unit.convert(that.value, that.unit, Unit.TEA_SPOON);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(value, unit);
+    return Objects.hash(Unit.convert(this.value, this.unit, Unit.TEA_SPOON));
   }
 
   public String toString() {
     return String.format("%f %s", value, unit);
   }
+
 }
--- test/unit/MeasureTest.java
+++ test/unit/MeasureTest.java
@@ -2,18 +2,23 @@ package unit;
 
 import measures.Measure;
 import org.junit.jupiter.api.Test;
+import probability.Probability;
 
-import static measures.Measure.Unit.CUP;
-import static measures.Measure.Unit.GALLON;
-import static measures.Measure.Unit.OUNCE;
-import static measures.Measure.Unit.PINT;
-import static measures.Measure.Unit.QUART;
-import static measures.Measure.Unit.TABLE_SPOON;
-import static measures.Measure.Unit.TEA_SPOON;
+import java.util.Collections;
+import java.util.HashSet;
+
+import static measures.Unit.CUP;
+import static measures.Unit.GALLON;
+import static measures.Unit.OUNCE;
+import static measures.Unit.PINT;
+import static measures.Unit.QUART;
+import static measures.Unit.TABLE_SPOON;
+import static measures.Unit.TEA_SPOON;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MeasureTest {
+class MeasureTest {
 
   @Test
   void constructor() {
@@ -22,11 +27,18 @@ public class MeasureTest {
 
   @Test
   void compare() {
-    assertEquals(new Measure(1, TABLE_SPOON), new Measure(3, Measure.Unit.TEA_SPOON));
+    assertEquals(new Measure(1, TABLE_SPOON), new Measure(3, TEA_SPOON));
     assertEquals(new Measure(1, OUNCE), new Measure(2, TABLE_SPOON));
     assertEquals(new Measure(1, CUP), new Measure(8, OUNCE));
     assertEquals(new Measure(1, PINT), new Measure(2, CUP));
     assertEquals(new Measure(1, QUART), new Measure(2, PINT));
+    assertEquals(new Measure(1, GALLON), new Measure(4, QUART));
+
+    assertEquals(new Measure(1, GALLON), new Measure(768, TEA_SPOON));
+    assertEquals(new Measure(1, GALLON), new Measure(256, TABLE_SPOON));
+    assertEquals(new Measure(1, GALLON), new Measure(128, OUNCE));
+    assertEquals(new Measure(1, GALLON), new Measure(16, CUP));
+    assertEquals(new Measure(1, GALLON), new Measure(8, PINT));
     assertEquals(new Measure(1, GALLON), new Measure(4, QUART));
   }
 
@@ -37,10 +49,38 @@ public class MeasureTest {
 
     assertEquals(new Measure(1, GALLON), new Measure(768, TEA_SPOON).convertTo(GALLON));
     assertEquals(new Measure(768, TEA_SPOON), new Measure(1, GALLON).convertTo(TEA_SPOON));
+
+    assertEquals(new Measure(0.5, CUP), new Measure(4, OUNCE).convertTo(CUP));
+
   }
+
+  @Test
+  void testHashCode() {
+    assertEquals(new Measure(4, TEA_SPOON).hashCode(), new Measure(4, TEA_SPOON).hashCode());
+
+    assertEquals(new Measure(1, TABLE_SPOON).hashCode(), new Measure(3, TEA_SPOON).hashCode());
+
+    assertTrue(new HashSet<>(Collections.singleton(new Measure(1, TABLE_SPOON))).contains(new Measure(1, TABLE_SPOON)));
+    assertTrue(new HashSet<>(Collections.singleton(new Measure(1, TABLE_SPOON))).contains(new Measure(3, TEA_SPOON)));
+  }
+
+  @Test
+  void hashSet() {
+    final HashSet<Probability> hashSet = new HashSet<>();
+    hashSet.add(new Probability(0.82));
+    hashSet.add(new Probability(0.82));
+
+    assertEquals(1, hashSet.size());
+  }
+
 
   @Test
   void add() {
     assertEquals(new Measure(1, TABLE_SPOON), new Measure(1, TEA_SPOON).add(new Measure(2, TEA_SPOON), TABLE_SPOON));
+  }
+
+  @Test
+  void subtract() {
+    assertEquals(new Measure(192, TABLE_SPOON), new Measure(1, GALLON).subtract(new Measure(2, PINT), TABLE_SPOON));
   }
 }
