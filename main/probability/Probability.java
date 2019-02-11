0e2d42edbf4f44e49ff6a091825412c4bce478fc
u208490 <urs.beeli@sbb.ch>
2019-02-11 14:43:18 +0100

    probability exercise done

--- main/probability/Probability.java
+++ main/probability/Probability.java
@@ -3,21 +3,46 @@ package probability;
 import java.util.Objects;
 
 public class Probability {
-  private final double probability;
+  private static final double CERTAINTY = 1.0;
+  private static final double MAX_EPSILON = 0.0000000001;
 
-  public Probability(final double probability) {
-    if (probability < 0.0 || probability > 1.0) {
-      throw new IllegalArgumentException(String.format("Probability %f not in range [0.0 .. 1.0]", probability));
+  private final double likelyhoodFraction;
+
+  public Probability(final double likelyhoodFraction) {
+    if (likelyhoodFraction < 0.0 || likelyhoodFraction > 1.0) {
+      throw new IllegalArgumentException(String.format("Probability %f not in range [0.0 .. 1.0]", likelyhoodFraction));
     }
-    this.probability = probability;
+    this.likelyhoodFraction = likelyhoodFraction;
   }
 
   public boolean isMoreLikely(final Probability other) {
-    return probability > other.probability;
+    return likelyhoodFraction > other.likelyhoodFraction;
   }
 
   public boolean isLessLikely(final Probability other) {
-    return probability < other.probability;
+    return likelyhoodFraction < other.likelyhoodFraction;
+  }
+
+  public Probability and(final Probability other) {
+    return new Probability(likelyhoodFraction * other.likelyhoodFraction);
+  }
+
+  public Probability or(final Probability other) {
+    //return new Probability(likelyhoodFraction + other.likelyhoodFraction - and(other).likelyhoodFraction);
+    return this.not().and(other.not()).not();
+  }
+
+  public Probability not() {
+    return new Probability(CERTAINTY - likelyhoodFraction);
+  }
+
+  public Probability times(final int count) {
+    return new Probability(Math.pow(likelyhoodFraction, count));
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%f", likelyhoodFraction);
   }
 
   @Override
@@ -29,25 +54,12 @@ public class Probability {
       return false;
     }
     final Probability that = (Probability) other;
-    return Double.compare(that.probability, this.probability) == 0;
+    //return Double.compare(that.likelyhoodFraction, this.likelyhoodFraction) == 0; // this breaks on new Probability(0.3).not().not();
+    return Math.abs(that.likelyhoodFraction - this.likelyhoodFraction) < MAX_EPSILON;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(probability);
+    return Objects.hash(likelyhoodFraction);
   }
-
-
-  public Probability and(final Probability probability) {
-    return null;
-  }
-
-  public Probability or(final Probability other) {
-    return null;
-  }
-
-  public Probability not() {
-    return null;
-  }
-
 }
--- test/unit/ProbabilityTest.java
+++ test/unit/ProbabilityTest.java
@@ -1,23 +1,24 @@
 package unit;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import probability.Probability;
 
 import java.util.Collections;
 import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ProbabilityTest {
 
   @Test
   void constructorOutOfRange() {
-    assertThrows(IllegalArgumentException.class, () -> new Probability(-3));
+    assertThrows(IllegalArgumentException.class, () -> new Probability(-0.0001));
     assertThrows(IllegalArgumentException.class, () -> new Probability(1.000005));
   }
 
@@ -31,8 +32,8 @@ class ProbabilityTest {
 
   @Test
   void testHashCode() {
-    //assertEquals(new Probability(0.25).hashCode(), new Probability(0.25).hashCode());
-    assertTrue(new HashSet<Probability>(Collections.singleton(new Probability(0.45))).contains(new Probability(0.45)));
+    assertEquals(new Probability(0.25).hashCode(), new Probability(0.25).hashCode());
+    assertTrue(new HashSet<>(Collections.singleton(new Probability(0.45))).contains(new Probability(0.45)));
   }
 
   @Test
@@ -44,33 +45,68 @@ class ProbabilityTest {
     assertEquals(1, hashSet.size());
   }
 
-//  @Test
-//  void testLikelyhood() {
-//    assertFalse(new Probability(0.567).isMoreLikely(new Probability(0.8)));
-//    assertTrue(new Probability(0.567).isMoreLikely(new Probability(0.2)));
-//
-//    assertFalse(new Probability(0.567).isLessLikely(new Probability(0.8)));
-//    assertTrue(new Probability(0.567).isLessLikely(new Probability(0.2)));
-//  }
-//
-//  @Ignore
-//  @Test
-//  void testProbabilityAnd(){
-//    assertEquals(new Probability(0.24), new Probability(0.4).and(new Probability(0.6)));
-//    assertEquals(new Probability(0.06), new Probability(0.4).and(new Probability(0.5)).and(new Probability(0.3)));
-//  }
-//
-//  @Ignore
-//  @Test
-//  void testProbabilityOr(){
-//    assertEquals(new Probability(0.68), new Probability(0.6).or(new Probability(0.2)));
-//  }
-//
-//  @Ignore
-//  @Test
-//  void testNot() {
-//    assertEquals(new Probability(0.25), new Probability(0.75).not());
-//  }
-//
-//
+  @Test
+  void testLikelyhood() {
+    assertFalse(new Probability(0.567).isMoreLikely(new Probability(0.8)));
+    assertTrue(new Probability(0.567).isMoreLikely(new Probability(0.2)));
+
+    assertTrue(new Probability(0.567).isLessLikely(new Probability(0.8)));
+    assertFalse(new Probability(0.567).isLessLikely(new Probability(0.2)));
+  }
+
+  @Test
+  void testProbabilityAnd() {
+    assertEquals(new Probability(0.0), new Probability(0.0).and(new Probability(1.0)));
+
+    assertEquals(new Probability(0.0), new Probability(0.0).and(new Probability(0.6)));
+    assertEquals(new Probability(0.0), new Probability(0.0).and(new Probability(1.0)));
+
+    assertEquals(new Probability(0.4), new Probability(1.0).and(new Probability(0.4)));
+    assertEquals(new Probability(0.6), new Probability(1.0).and(new Probability(0.6)));
+
+    assertEquals(new Probability(0.24), new Probability(0.4).and(new Probability(0.6)));
+    assertEquals(new Probability(0.24), new Probability(0.6).and(new Probability(0.4)));
+
+    assertEquals(new Probability(0.06),
+        new Probability(0.4)
+            .and(new Probability(0.5))
+            .and(new Probability(0.3)));
+
+    assertEquals(new Probability(0.06),
+        new Probability(0.4)
+            .and(
+                new Probability(0.5)
+                    .and(new Probability(0.3))));
+
+    assertThat(new Probability(0.4).and(new Probability(0.6))).isEqualTo(new Probability(0.24));
+  }
+
+  @Test
+  void testProbabilityOr() {
+    assertEquals(new Probability(1.0), new Probability(0.0).or(new Probability(1.0)));
+
+    assertEquals(new Probability(0.6), new Probability(0.0).or(new Probability(0.6)));
+    assertEquals(new Probability(0.2), new Probability(0.0).or(new Probability(0.2)));
+
+    assertEquals(new Probability(1.0), new Probability(1.0).or(new Probability(0.6)));
+    assertEquals(new Probability(1.0), new Probability(1.0).or(new Probability(0.2)));
+
+    assertEquals(new Probability(0.68), new Probability(0.6).or(new Probability(0.2)));
+    assertEquals(new Probability(0.68), new Probability(0.6).or(new Probability(0.2)));
+  }
+
+  @Test
+  void testNot() {
+    assertEquals(new Probability(0.25), new Probability(0.75).not());
+    assertEquals(new Probability(0.0), new Probability(1.0).not());
+    assertEquals(new Probability(1.0), new Probability(0.0).not());
+    assertEquals(new Probability(0.25), new Probability(0.25).not().not());
+    assertEquals(new Probability(0.3), new Probability(0.3).not().not());
+    assertNotEquals(new Probability(0.25), new Probability(0.25).not());
+  }
+
+  @Test
+  void times() {
+    assertEquals(new Probability(0.00001), new Probability(0.1).times(5));
+  }
 }
