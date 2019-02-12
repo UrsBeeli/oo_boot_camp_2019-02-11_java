7465aedb8b4456b35613cb0d7c3935f7df5f8fa0
u208490 <urs.beeli@sbb.ch>
2019-02-12 16:47:02 +0100

    cube inches to teaspon factor added

--- main/measures/Volume.java
+++ main/measures/Volume.java
@@ -9,13 +9,12 @@ public class Volume extends BaseUnit {
   public static final Volume QUART = new Volume(2, PINT);
   public static final Volume GALLON = new Volume(4, QUART);
 
-  public static final Volume CUBE_INCH = new Volume(12345, TEASPOON);  // correct factor to be determined
+  public static final Volume CUBE_INCH = new Volume(3.3246763996984, TEASPOON);  // ratio according to google
   public static final Volume CUBE_FOOT = new Volume(12 * 12 * 12, CUBE_INCH);
   public static final Volume CUBE_YARD = new Volume(3 * 3 * 3, CUBE_FOOT);
   public static final Volume CUBE_CHAIN = new Volume(22 * 22 * 22, CUBE_YARD);
   public static final Volume CUBE_FURLONG = new Volume(10 * 10 * 10, CUBE_CHAIN);
   public static final Volume CUBE_MILE = new Volume(8 * 8 * 8, CUBE_FURLONG);
-
 
   private Volume() {
     super();
