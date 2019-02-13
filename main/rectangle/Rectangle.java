8197ce08821d6956add8aa7f81db79ce04b4249a
u208490 <urs.beeli@sbb.ch>
2019-02-13 11:31:58 +0100

    square

--- main/rectangle/Rectangle.java
+++ main/rectangle/Rectangle.java
@@ -6,11 +6,23 @@ public class Rectangle {
   private final double width;
 
   public Rectangle(final double height, final double width) {
+    if (height <= 0 || width <= 0) {
+      throw new IllegalArgumentException("Parameter(s) must be positive numbers");
+    }
+
     this.height = height;
     this.width = width;
   }
 
   public double area() {
     return height * width;
+  }
+
+  public double circumference() {
+    return 2 * (height + width);
+  }
+
+  public static Rectangle square(double length) {
+    return new Rectangle(length, length);
   }
 }
--- test/unit/RectangleTest.java
+++ test/unit/RectangleTest.java
@@ -8,7 +8,8 @@ package unit;
 import org.junit.jupiter.api.Test;
 import rectangle.Rectangle;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 // Ensures Rectangle operates correctly
 class RectangleTest {
@@ -17,7 +18,32 @@ class RectangleTest {
         assertEquals(24.0, new Rectangle(4.0, 6.0).area());
     }
 
-    @Test() void notNegative(){
-        new Rectangle(-1, 0);
+    @Test()
+    void notNullNegative() {
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(-1, 5));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(1, -5));
+
+        assertThrows(IllegalArgumentException.class, () -> Rectangle.square(-5));
+
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(3, 0));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(0, 5));
+        assertThrows(IllegalArgumentException.class, () -> Rectangle.square(0));
+
     }
+
+    @Test
+    void circumference() {
+        assertEquals(30, new Rectangle(10, 5).circumference());
+    }
+
+    @Test
+    void square() {
+        assertEquals(16, Rectangle.square(4).area());
+        assertEquals(25, Rectangle.square(5).area());
+
+        assertEquals(16, Rectangle.square(4).circumference());
+        assertEquals(20, Rectangle.square(5).circumference());
+    }
+
+
 }
